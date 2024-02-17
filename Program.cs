d62c49385cea98b05c2d42e2a1fe0ee3399395c5
romulodeoliveira <devromulodeoliveira@gmail.com>
2024-02-17 15:50:37 -0300

    adicionando referencias, documentando a api, configurando o CORS, registrando endpoints

--- Program.cs
+++ Program.cs
@@ -1,4 +1,24 @@
+/*
+ * DOCUMENTAÇÃO LIDA PARA ESTE PROJETO
+ *
+ * Tutorial: Criar uma API mínima com o ASP.NET Core - MICROSOFT
+ * https://learn.microsoft.com/pt-br/aspnet/core/tutorials/min-web-api?view=aspnetcore-8.0&tabs=visual-studio
+ *
+ * ASP.NET Minimal APIs - Por André Baltieri
+ * https://balta.io/blog/aspnet-minimal-apis
+ *
+ * Novidades do .NET 7: Route Groups para organização de endpoints em Minimal APIs - Por Renato Groffe
+ * https://renatogroffe.medium.com/novidades-do-net-7-route-groups-para-organiza%C3%A7%C3%A3o-de-endpoints-em-minimal-apis-c78c64b49dfa
+ *
+ * Simplifying Routes in .NET Core with Route Grouping: A Minimal API Approach - Por Sheldon Cohen
+ * https://sheldonrcohen.medium.com/simplifying-routes-in-net-core-with-route-grouping-a-minimal-api-approach-b98ec8e53cd9
+ * 
+ * Minimal APIs quick reference - MICROSOFT
+ * https://learn.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis?view=aspnetcore-7.0
+ */
+
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Phonebook.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,11 +26,35 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// >>> Documentação da API
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "API Agenda Telefonica",
+            Description = "Exemplo de implementação de Minimal API para Agenda Telefonica", 
+            Version = "v1",
+            Contact = new OpenApiContact()
+            {
+                Name = "Romulo de Oliveira",
+                Email = "dev@romulodeoliveira.net",
+                Url = new Uri("https://github.com/romulodeoliveira"),
+            },
+            License = new OpenApiLicense()
+            {
+                Name = "MIT",
+                Url = new Uri("http://opensource.org/licenses/MIT"),
+            }
+        });
+});
 
-// Database config
+// >>> Configuração do Banco de Dados
 string dbConfig = "Data Source=data.db;";
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(dbConfig));
+
+// >>> Configuração de solicitações HTTP
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -18,8 +62,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Phonebook v1");
+    });
 }
+
+// >>> Configuração de solicitações HTTP
+app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.UseHttpsRedirection();
 
@@ -42,6 +92,13 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+// >>> Registrando os endpoints
+var contactEndpoints = new Phonebook.Endpoints.Contact();
+contactEndpoints.RegisterEndpoints(app);
+
+var userEndpoints = new Phonebook.Endpoints.User();
+userEndpoints.RegisterEndpoints(app);
 
 app.Run();
 
