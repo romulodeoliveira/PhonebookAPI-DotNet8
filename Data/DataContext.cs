27c13a9cb230e7a25e33db2c5a577cd7075375c7
romulodeoliveira <devromulodeoliveira@gmail.com>
2024-02-18 18:15:03 -0300

    desenvolvendo a relação de muitos contatos para um usuário

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,4 +8,14 @@ public class DataContext : DbContext
 
     public DbSet<Models.Contact> Contacts { get; set; }
     public DbSet<Models.User> Users { get; set; }
+    
+    // Configurando a relação de muitos contatos para um usuário
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Models.Contact>()
+            .HasOne(c => c.User)
+            .WithMany(u => u.Contacts)
+            .HasForeignKey(c => c.UserId)
+            .IsRequired();
+    }
 }
